3619d5dbcf47facc8205961d6d9bc65cab41aa7d
USER <USER@AMP-00>
2019-03-22 10:23:15 +0900

    Commit

--- chap04_ex03/src/chap04_ex03/Circle.java
+++ chap04_ex03/src/chap04_ex03/Circle.java
@@ -1,9 +1,36 @@
 package chap04_ex03;
 
 public class Circle {
+	int radius;
+	String name;
+
+	public Circle() {
+		radius = 1;
+		name = "";
+
+	}
+
+	public Circle(int r, String n) {
+		radius = r;
+		name = n;
+
+	}
+
+	public double getArea() {
+		return 3.14 * radius * radius;
+
+	}
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
+		Circle pizza = new Circle(10, "Ã¥À»ÇÇÀÚ");
+		double area = pizza.getArea();
+		System.out.println(pizza.name + area);
+
+		Circle donut = new Circle();
+		donut.name = "Èñ¼ºµµ³Ó";
+		area = donut.getArea();
+		System.out.println(donut.name + area);
 
 	}
 
