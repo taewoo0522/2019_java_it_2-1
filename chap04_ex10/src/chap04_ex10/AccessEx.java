1a06894d807903073e2d4cad1dc2424edcde4fd6
USER <USER@DESKTOP-JQT23F0>
2019-04-18 12:11:02 +0900

    Commit

---
+++ chap04_ex10/src/chap04_ex10/AccessEx.java
@@ -0,0 +1,13 @@
+package chap04_ex10;
+
+public class AccessEx {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Sample sample = new Sample();
+		sample.a = 10;
+		sample.b = 10;
+		sample.c = 10;
+	}
+
+}
---
+++ chap04_ex10/src/chap04_ex10/Sample.java
@@ -0,0 +1,8 @@
+package chap04_ex10;
+
+class Sample {
+	public int a;
+	private int b; // 같은 크래스 내부에서만 접근이 가능함.
+	int c; // 디폴트 접근 지정
+
+}
